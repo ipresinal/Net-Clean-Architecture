9af884d6b7aa6986711118c507802d75339789d8
ipresinal <ivan.presinal@hotmail.com>
2018-11-15 11:28:06 -0400

    building architecture

---
+++ NetCleanArchitecture/CustomerApp.Core/DomainService/ICustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+using CustomerApp.Core.Entity;
+
+namespace CustomerApp.Core.DomainService
+{
+    public interface ICustomerRepository
+    {
+        // Customer Repository Interface
+
+        // Create Data
+        // No Id when enter, with Id when exist
+        Customer Create(Customer customer);
+
+        // Read Data
+        Customer ReadById(int id);
+        List<Customer> ReadAll();
+
+        // Update Data
+        Customer Update(Customer customerUpdate);
+
+        // Delete Data
+        Customer Delete(int id);
+
+    }
+}
---
+++ NetCleanArchitecture/CustomerApp.Infrastructure.Static.Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomerApp.Core.DomainService;
+using CustomerApp.Core.Entity;
+
+namespace CustomerApp.Infrastructure.Static.Data.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private int id = 1;
+        private List<Customer> _customers = new List<Customer>();    
+
+        public Customer Create(Customer customer)
+        {
+            customer.Id = id++;
+            _customers.Add(customer);
+            return customer;
+
+        }
+       
+        public List<Customer> ReadAll()
+        {
+            return _customers;
+        }
+
+        public Customer ReadById(int id)
+        {
+            foreach (var customer in _customers)
+            {
+                if (customer.Id == id)
+                {
+                    return customer;
+                }
+            }
+            return null;
+        }
+
+
+        /// <summary>
+        /// Remove later when we use UOW
+        /// </summary>
+        /// <param name="customerUpdate"></param>
+        /// <returns></returns>
+        public Customer Update(Customer customerUpdate)
+        {
+            var customerFromDB = ReadById(customerUpdate.Id);
+
+            if (customerFromDB == null) return null;
+
+            customerFromDB.FirstName = customerUpdate.FirstName;
+            customerFromDB.LastName = customerUpdate.LastName;
+            customerFromDB.Address = customerUpdate.Address;
+
+            return customerFromDB;
+
+        }
+
+        public Customer Delete(int id)
+        {
+            var customerFound = ReadById(id);
+            if (customerFound == null) return null;
+
+            _customers.Remove(customerFound);
+            return customerFound;
+        }
+    }
+}
